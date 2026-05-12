2465d30761eb9eabc1a4d55b9b4a16b71a17076e
Anton Usanov <anton.usanov.kb.2023@lpnu.ua>
2026-05-12 16:30:42 +0300

    test auto deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="bg-white/20 backdrop-blur-lg text-white p-8 rounded-2xl shadow-xl text-center border border-white/30">
-      <h1 className="text-4xl font-bold mb-2">Антон Усанов Олександрович!!!</h1>
+      <h1 className="text-4xl font-bold mb-2">Антон Усанов Олександрович</h1>
       <p className="text-lg opacity-90">Студент Національного університету Львівська політехніка</p>
     </header>
   );
